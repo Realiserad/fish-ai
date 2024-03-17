0585d156891b5b4fb16c5e20a0ca02b3fcc25143
Bastian Fredriksson <bastian.fredriksson@keyfactor.com>
2024-03-17 23:42:19 +0100

    fix: autocompletion
    Autocompletion not working when output contain command line'
    flags. Do not autocomplete empty prompts.

--- functions/_fish_ai_autocomplete_or_fix.fish
+++ functions/_fish_ai_autocomplete_or_fix.fish
@@ -2,16 +2,16 @@
 
 function _fish_ai_autocomplete_or_fix --description "Autocomplete the current command or fix the previous command using AI."
     set previous_status $status
+    set input (commandline --current-buffer)
     if test -z "$input" && test $previous_status -ne 0
         # Fix the previous command.
         set previous_command (history | head -1)
         set fixed_command (_fish_ai_fix "$previous_command")
         commandline --replace "$fixed_command"
-    else
+    else if test -n "$input"
         # Autocomplete the current command
-        set input (commandline --current-buffer)
         set cursor_position (commandline --cursor)
         set output (_fish_ai_autocomplete "$input" "$cursor_position")
-        commandline --insert "$output"
+        commandline --insert " $output"
     end
 end
