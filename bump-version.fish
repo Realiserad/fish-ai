ead645b935610e69c1f7429374c05e8143fb632e
Bastian Fredriksson <bastian.fredriksson@keyfactor.com>
2024-04-07 22:57:32 +0200

    ci: bump version using hook

---
+++ bump-version.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+set current_branch (git rev-parse --abbrev-ref HEAD)
+set start_hash (git show-ref --hash refs/remotes/origin/$current_branch)
+set current_version (grep version pyproject.toml | head -n 1 | cut -d'"' -f2)
+set next_version (what-bump --from $current_version $start_hash)
+sed -i -E "s/version = .+/version = \"$next_version\"/" pyproject.toml
+git add pyproject.toml
+git commit -C HEAD --amend
