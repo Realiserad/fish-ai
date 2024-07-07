7b160fc5ffd5fcd88c6cbadbf2cb2e979f7582e4
Bastian Fredriksson <bastian.fredriksson@keyfactor.com>
2024-07-07 12:48:44 +0200

    feat: redact sensitive information
    Redact sensitive information from the prompt before sending it to the LLM, such as passwords and private keys.

---
+++ src/fish_ai/redact.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+
+def redact(messages):
+    for message in messages:
+        message['content'] = redact_content(message['content'])
+    return messages
+
+
+def redact_content(content):
+    r = content
+    r = redact_cli_parameter('api-key', r)
+    r = redact_cli_parameter('password', r)
+    r = redact_cli_parameter('passphrase', r)
+    r = redact_cli_parameter('secret', r)
+    r = redact_pem_encoded_private_key(r)
+    r = redact_pem_encoded_private_key_block(r)
+    r = redact_base64_data(r)
+    return r
+
+
+def redact_cli_parameter(param, content):
+    pattern = r'--{param}([= ]?)(["\']?)\S+\2'.format(
+        param=param)
+    replace_with = r'--{param}\1\2<REDACTED>\2'.format(
+        param=param)
+    return re.sub(
+        pattern,
+        replace_with,
+        content)
+
+
+def redact_pem_encoded_private_key(content):
+    pattern = (r'-----BEGIN ([A-Z0-9]+) PRIVATE KEY-----\n'
+               r'[\s\S]+\n'
+               r'-----END \1 PRIVATE KEY-----')
+    replace_with = (r'-----BEGIN \1 PRIVATE KEY-----\n'
+                    r'<REDACTED>\n'
+                    r'-----END \1 PRIVATE KEY-----')
+    return re.sub(
+        pattern,
+        replace_with,
+        content,
+        flags=re.DOTALL)
+
+
+def redact_pem_encoded_private_key_block(content):
+    pattern = (r'-----BEGIN ([A-Z0-9]+) PRIVATE KEY BLOCK-----\n'
+               r'[\s\S]+\n'
+               r'-----END \1 PRIVATE KEY BLOCK-----')
+    replace_with = (r'-----BEGIN \1 PRIVATE KEY BLOCK-----\n'
+                    r'<REDACTED>\n'
+                    r'-----END \1 PRIVATE KEY BLOCK-----')
+    return re.sub(
+        pattern,
+        replace_with,
+        content,
+        flags=re.DOTALL)
+
+
+def redact_base64_data(content):
+    pattern = r'["\']([A-Za-z0-9+\\/=]+={0,2})["\']'
+    replace_with = r'"<REDACTED>"'
+    return re.sub(
+        pattern,
+        replace_with,
+        content)
---
+++ src/fish_ai/tests/redact_test.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+
+from fish_ai.redact import redact_content
+import textwrap
+
+
+def test_redact_api_key():
+    input_str = '--api-key=sk-1234'
+    expected_output = '--api-key=<REDACTED>'
+    assert redact_content(input_str) == expected_output
+
+    input_str = '--api-key sk-1234'
+    expected_output = '--api-key <REDACTED>'
+    assert redact_content(input_str) == expected_output
+
+    input_str = "--api-key 'sk-1234'"
+    expected_output = "--api-key '<REDACTED>'"
+    assert redact_content(input_str) == expected_output
+
+    input_str = '--api-key "sk-1234"'
+    expected_output = '--api-key "<REDACTED>"'
+    assert redact_content(input_str) == expected_output
+
+
+def test_redact_password():
+    input_str = 'login --username foo --password=samba123'
+    expected_output = 'login --username foo --password=<REDACTED>'
+    assert redact_content(input_str) == expected_output
+
+    input_str = 'login --username foo --password samba123'
+    expected_output = 'login --username foo --password <REDACTED>'
+    assert redact_content(input_str) == expected_output
+
+    input_str = "login --username foo --password 'samba123'"
+    expected_output = "login --username foo --password '<REDACTED>'"
+    assert redact_content(input_str) == expected_output
+
+    input_str = 'login --username foo --password "samba123!"'
+    expected_output = 'login --username foo --password "<REDACTED>"'
+    assert redact_content(input_str) == expected_output
+
+
+def test_redact_multiple_cli_parameters():
+    input_str = textwrap.dedent("""\
+        Here are some login commands for cucumber, tomato and pepper:
+
+        login --username cucumber --password cucumber
+        login --username tomato --api-key tomato
+        echo 'cGVwcGVy' | login --username pepper""")
+    expected_output = textwrap.dedent("""\
+        Here are some login commands for cucumber, tomato and pepper:
+
+        login --username cucumber --password <REDACTED>
+        login --username tomato --api-key <REDACTED>
+        echo \"<REDACTED>\" | login --username pepper""")
+    assert redact_content(input_str) == expected_output
+
+
+def test_redact_base64_data():
+    input_str = 'echo "cGFzc3dvcmQxMjM=" | login'
+    expected_output = 'echo "<REDACTED>" | login'
+    assert redact_content(input_str) == expected_output
+
+    input_str = "echo 'cGFzc3dvcmQxMjM=' | login"
+    expected_output = 'echo "<REDACTED>" | login'
+    assert redact_content(input_str) == expected_output
+
+
+def test_redact_pem_encoded_private_key():
+    input_str = textwrap.dedent("""\
+        -----BEGIN RSA PRIVATE KEY-----
+        Proc-Type: 4,ENCRYPTED
+        DEK-Info: DES-EDE3-CBC,B1F1B3F5F1B4F1B3
+        6+jVglcOq6vNfwt/Q+X9m
+        -----END RSA PRIVATE KEY-----""")
+    expected_output = textwrap.dedent("""\
+        -----BEGIN RSA PRIVATE KEY-----
+        <REDACTED>
+        -----END RSA PRIVATE KEY-----""")
+    assert redact_content(input_str) == expected_output
+
+
+def test_redact_content():
+    input_str = textwrap.dedent("""\
+        Autocomplete the following command:
+
+        key import --file key.pem --password samba123
+
+        You may use the following command line history to personalize the
+        response:
+
+        key import --file key.pem --password samba123 --server server1.com
+        key import --file key.pem --password samba123 --server server2.com
+
+        The content of key.pem is:
+
+        -----BEGIN PGP PRIVATE KEY BLOCK-----
+        123456789123456789012345678901234567890123456789012345678901234
+        123456789123456789012345678901234567890123456789012345678901234
+        123456789123456789012345678901234567890123456789012345678901234
+        -----END PGP PRIVATE KEY BLOCK-----""")
+    expected_output = textwrap.dedent("""\
+        Autocomplete the following command:
+
+        key import --file key.pem --password <REDACTED>
+
+        You may use the following command line history to personalize the
+        response:
+
+        key import --file key.pem --password <REDACTED> --server server1.com
+        key import --file key.pem --password <REDACTED> --server server2.com
+
+        The content of key.pem is:
+
+        -----BEGIN PGP PRIVATE KEY BLOCK-----
+        <REDACTED>
+        -----END PGP PRIVATE KEY BLOCK-----""")
+    assert redact_content(input_str) == expected_output
+
+
+def test_nothing_to_redact():
+    input_str = 'Nothing to redact here...'
+    assert redact_content(input_str) == input_str
