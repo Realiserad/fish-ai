ab3ba6b446b50393c0b8b711f722c186ae5f39c2
Bastian Fredriksson <bastian.fredriksson@keyfactor.com>
2024-10-13 18:05:10 +0200

    fix: missing lookup_setting during upgrade

--- functions/_fish_ai_codify_or_explain.fish
+++ functions/_fish_ai_codify_or_explain.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
-if test (~/.fish-ai/bin/lookup_setting status_emoji) != False
-    eval "function fish_right_prompt; echo (string escape \$status_emoji); end"
-end
-
 function _fish_ai_codify_or_explain --description "Transform a command into a comment and vice versa using AI."
+    if test (~/.fish-ai/bin/lookup_setting status_emoji) != False
+        eval "function fish_right_prompt; echo (string escape \$status_emoji); end"
+    end
+
     set input (commandline --current-buffer)
 
     if test -z "$input"
