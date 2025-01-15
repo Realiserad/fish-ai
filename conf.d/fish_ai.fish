2dba59578af0164039668590b725de7982bfe59d
Bastian Fredriksson <bastian.fredriksson@keyfactor.com>
2025-01-15 15:03:17 -0500

    fix: avoid crash when fish_right_prompt is undefined
    Closes: #142
    
    Signed-off-by: Bastian Fredriksson <bastian.fredriksson@keyfactor.com>

--- conf.d/fish_ai.fish
+++ conf.d/fish_ai.fish
@@ -145,7 +145,12 @@ function autoconfig_gh_models --description "Deploy configuration for GitHub Mod
 end
 
 function show_progess_indicator --description "Show a progress indicator."
+    if type -q fish_right_prompt
+        set rplen (string length -v (fish_right_prompt)[-1])
+    else
+        set rplen 0
+    end
     # Move the cursor to the end of the line and insert progress indicator
-    tput hpa (math $COLUMNS - (string length -v (fish_right_prompt)[-1]) - 2)
+    tput hpa (math $COLUMNS - $rplen - 2)
     echo -n '⏳'
 end
