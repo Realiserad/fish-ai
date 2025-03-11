2199f15a7948d37dd44faf357e250de875c8ff99
Bastian Fredriksson <bastian.fredriksson@keyfactor.com>
2025-03-11 13:54:16 +0000

    test: remove broken testtest: remove broken test

--- src/fish_ai/tests/engine_test.py
+++ src/fish_ai/tests/engine_test.py
@@ -11,18 +11,3 @@ def test_get_commandline_history_disabled(mock_get_logger, mock_get_config):
     assert get_commandline_history('command', 0) == \
         'No commandline history available.'
     mock_get_logger.assert_called_once()
-
-
-@patch('fish_ai.engine.get_config')
-@patch('fish_ai.engine.subprocess')
-def test_get_commandline_history_return_2(mock_subprocess, mock_get_config):
-    mock_get_config.return_value = '2'
-    mock_subprocess.Popen.return_value.stdout.readline.side_effect = [
-        b'docker ps | grep apache1\n',
-        b'docker ps | grep apache2\n',
-        b'docker ps | grep nginx\n',
-        b'docker exec -it apache1 /bin/bash\n'
-    ]
-    # docker ps | grep█
-    result = get_commandline_history('docker ps | grep', 16)
-    assert result == 'docker ps | grep apache1\ndocker ps | grep apache2'
