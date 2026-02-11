47fd24aa70ae3b7aaf17ec0f1e482fbcb7d2acb2
Bastian Fredriksson <realiserad@gmail.com>
2026-02-11 15:55:41 -0500

    fix: more robust check if the commandline is a "comment"
    Avoid the error "expected a combining operator like '-a' at index" if e.g. the commandline starts with an empty line.

--- functions/_fish_ai_codify_or_explain.fish
+++ functions/_fish_ai_codify_or_explain.fish
@@ -9,7 +9,7 @@ function _fish_ai_codify_or_explain --description "Transform a command into a co
 
     _fish_ai_show_progress_indicator
 
-    if test (string sub --length 2 "$input") = "# "
+    if string match -q "# *" (string trim "$input")
         set -f output (_fish_ai_codify "$input" | string collect)
     else
         set -f output (_fish_ai_explain "$input" | string collect)
