2c2bce932612a5be6bccf0a5ddbd5cbe2f472073
Bastian Fredriksson <realiserad@gmail.com>
2026-05-18 00:17:28 +0000

    refactor: resolve issues flagged by basedpyright

--- src/fish_ai/switch_context.py
+++ src/fish_ai/switch_context.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from simple_term_menu import TerminalMenu
 from configparser import ConfigParser
+
+from simple_term_menu import TerminalMenu
+
 from fish_ai.config import get_config_path
 
 
@@ -9,17 +11,17 @@ def switch_context():
     config = ConfigParser()
     config.read(get_config_path())
     sections = config.sections()
-    sections.remove("fish-ai")
+    if "fish-ai" in sections:
+        sections.remove("fish-ai")
     options = [
         "{} (provided by {})".format(
             section, config.get(section=section, option="provider")
         )
         for section in sections
     ]
-    terminal_menu = TerminalMenu(options)
-    terminal_menu.title = "Select context"
+    terminal_menu = TerminalMenu(options, title="Select context")
     index = terminal_menu.show()
-    if index is None:
+    if index is None or isinstance(index, tuple):
         return
     active_section = options[index].split(" ")[0]
     config.set(section="fish-ai", option="configuration", value=active_section)
